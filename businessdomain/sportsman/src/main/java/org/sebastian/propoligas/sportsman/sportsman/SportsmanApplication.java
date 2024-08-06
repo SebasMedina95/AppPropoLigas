97f4db5975fca5fd0a36aa149d92717fd6325392
Sebastian Medina <jsebastian19952011@gmail.com>
2024-08-06 09:43:57 -0500

    Ajustes documentación Swagger

--- businessdomain/sportsman/src/main/java/org/sebastian/propoligas/sportsman/sportsman/SportsmanApplication.java
+++ businessdomain/sportsman/src/main/java/org/sebastian/propoligas/sportsman/sportsman/SportsmanApplication.java
@@ -3,9 +3,11 @@ package org.sebastian.propoligas.sportsman.sportsman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
 
 @EnableFeignClients
 @SpringBootApplication
+@PropertySource(value = "classpath:swagger.properties", encoding = "UTF-8")
 public class SportsmanApplication {
 
 	public static void main(String[] args) {
