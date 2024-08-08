a43001e7a1ee645acea1ec34ee8064e22abb64b3
Sebastian Medina <jsebastian19952011@gmail.com>
2024-08-07 21:05:00 -0500

    Configuración del configservice para centralizar los archivos properties para el manejo en las APIS - Implementación de mecánica para la infraestructura

---
+++ infraestructuredomain/configserver/src/test/java/org/sebastian/propoligas/configserver/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebastian.propoligas.configserver.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
